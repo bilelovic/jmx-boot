010663db40168ff6b2f76fa52be4eb3b059be645
bmemdouh <bilel.memdouh@savoirfairelinux.com>
2017-05-23 10:43:28 -0400

    Initial commit

---
+++ src/main/java/com/bilelovic/jmx/JmxApplication.java
@@ -0,0 +1,12 @@
+package com.bilelovic.jmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmxApplication.class, args);
+	}
+}
